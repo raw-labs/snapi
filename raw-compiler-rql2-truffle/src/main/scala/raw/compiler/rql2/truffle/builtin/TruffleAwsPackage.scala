b1a9aa8ce9790cf0d4ef0f1578339bca5157cf4e
alexzerntev <alexzerntev@gmail.com>
2023-07-05 19:14:54 +0300

    Finished the implementation, taking 404 from aws, needs fixing

--- raw-compiler-rql2-truffle/src/main/scala/raw/compiler/rql2/truffle/builtin/TruffleAwsPackage.scala
+++ raw-compiler-rql2-truffle/src/main/scala/raw/compiler/rql2/truffle/builtin/TruffleAwsPackage.scala
@@ -1,14 +1,55 @@
 package raw.compiler.rql2.truffle.builtin
 
 import raw.compiler.base.source.Type
-import raw.compiler.rql2.builtin.{AwsPackage, AwsV4SignedRequest}
+import raw.compiler.rql2.builtin.AwsV4SignedRequest
+import raw.compiler.rql2.source.{Rql2ListType, Rql2RecordType, Rql2StringType}
 import raw.compiler.rql2.truffle.{TruffleArg, TruffleEntryExtension}
 import raw.runtime.truffle.ExpressionNode
+import raw.runtime.truffle.ast.expressions.binary.PlusNode
+import raw.runtime.truffle.ast.expressions.builtin.aws_package.AwsV4SignedRequestNodeGen
+import raw.runtime.truffle.ast.expressions.iterable.list.ListBuildNode
+import raw.runtime.truffle.ast.expressions.literals.StringNode
 
 class AwsV4SignedRequestEntry extends AwsV4SignedRequest with TruffleEntryExtension {
 
   override def toTruffle(t: Type, args: Seq[TruffleArg]): ExpressionNode = {
 
+    val key = args.head.e
+    val secretKey = args(1).e
+    val service = args(2).e
+
+    val maybeRegion = args.collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "region" => e }
+
+    val method =
+      args.collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "method" => e }.getOrElse(new StringNode("GET"))
+
+    val host = args
+      .collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "host" => e }
+      .getOrElse(
+        if (maybeRegion.isDefined) new PlusNode(
+          new PlusNode(new PlusNode(service, new StringNode(".")), maybeRegion.get),
+          new StringNode(".amazonaws.com")
+        )
+        else new PlusNode(new PlusNode(service, new StringNode(".")), new StringNode("amazonaws.com"))
+      )
+
+    val path =
+      args.collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "path" => e }.getOrElse(new StringNode("/"))
+
+    val body =
+      args.collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "bodyString" => e }.getOrElse(new StringNode(""))
+
+    val urlParams = args
+      .collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "args" => e }
+      .getOrElse(new ListBuildNode(Rql2ListType(Rql2RecordType(Vector.empty)), Array()))
+
+    val headers = args
+      .collectFirst { case TruffleArg(e, _, Some(idn)) if idn == "headers" => e }
+      .getOrElse(new ListBuildNode(Rql2ListType(Rql2RecordType(Vector.empty)), Array()))
+
+    val region = maybeRegion.getOrElse(new StringNode("us-east-1"))
+
+    AwsV4SignedRequestNodeGen.create(key, secretKey, service, region, path, method, host, body, urlParams, headers)
   }
 
 }
--- raw-runtime-rql2-truffle/src/main/java/raw/runtime/truffle/ast/expressions/builtin/aws_package/AwsV4SignedRequestNode.java
+++ raw-runtime-rql2-truffle/src/main/java/raw/runtime/truffle/ast/expressions/builtin/aws_package/AwsV4SignedRequestNode.java
@@ -1,17 +1,37 @@
 package raw.runtime.truffle.ast.expressions.builtin.aws_package;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.nodes.NodeInfo;
 import raw.runtime.truffle.ExpressionNode;
+import raw.runtime.truffle.runtime.exceptions.RawTruffleInternalErrorException;
+import raw.runtime.truffle.runtime.list.ListLibrary;
 import raw.runtime.truffle.runtime.primitives.LocationObject;
+import raw.sources.LocationKVSetting;
+import raw.sources.LocationSettingKey;
+import raw.sources.LocationSettingValue;
+import raw.sources.LocationStringSetting;
+import scala.Tuple2;
+import scala.collection.immutable.HashMap;
+import scala.collection.immutable.Map;
+import scala.collection.immutable.VectorBuilder;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 @NodeInfo(shortName = "Aws.V4SignedRequest")
 @NodeChild("key")
@@ -22,7 +42,7 @@ import java.security.NoSuchAlgorithmException;
 @NodeChild("method")
 @NodeChild("host")
 @NodeChild("bodyString")
-@NodeChild("args")
+@NodeChild("urlParams")
 @NodeChild("headers")
 public abstract class AwsV4SignedRequestNode extends ExpressionNode {
 
@@ -47,12 +67,30 @@ public abstract class AwsV4SignedRequestNode extends ExpressionNode {
 
     private String toHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder(bytes.length * 2);
-        for(byte b: bytes)
+        for (byte b : bytes)
             sb.append(String.format("%02x", b));
         return sb.toString();
     }
 
-    @Specialization
+    // Amazon needs timestamps for signing requests with specific formats.
+    private DateTimeFormatter formatterWithTimeZone() {
+        return DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssX").withZone(ZoneId.from(ZoneOffset.UTC));
+    }
+
+    private DateTimeFormatter getDateFormatter() {
+        return DateTimeFormatter.ofPattern("yyyyMMdd").withZone(ZoneId.from(ZoneOffset.UTC));
+    }
+
+    private MessageDigest getSha256Digest() {
+        try {
+            return MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RawTruffleInternalErrorException(e);
+        }
+    }
+
+    @CompilerDirectives.TruffleBoundary
+    @Specialization(limit = "2")
     protected LocationObject doRequest(
             String key,
             String secretKey,
@@ -62,10 +100,119 @@ public abstract class AwsV4SignedRequestNode extends ExpressionNode {
             String method,
             String host,
             String bodyString,
-            Object args,
-            Object headers
+            Object urlParams,
+            Object headers,
+            @CachedLibrary("urlParams") ListLibrary urlParamsLists,
+            @CachedLibrary("headers") ListLibrary headersLists,
+            @CachedLibrary(limit = "2") InteropLibrary records
     ) {
+        try {
+            Instant t = Instant.now();
+            String amzdate = formatterWithTimeZone().format(t);
+            String datestamp = getDateFormatter().format(t);
 
-        return new LocationObject("TODO");
+            // Task 1: create canonical request with all request settings: method, canonicalUri, canonicalQueryString etc.
+
+            VectorBuilder<Tuple2<String, String>> urlParamsVec = new VectorBuilder<>();
+
+            StringBuilder canonicalQueryBuilder = new StringBuilder();
+            for (int i = 0; i < urlParamsLists.size(urlParams); i++) {
+                canonicalQueryBuilder
+                        .append(URLEncoder.encode((String) records.readMember(urlParamsLists.get(urlParams, i), "_1"), StandardCharsets.UTF_8))
+                        .append("=")
+                        .append(URLEncoder.encode((String) records.readMember(urlParamsLists.get(urlParams, i), "_2"), StandardCharsets.UTF_8))
+                        .append("&");
+                urlParamsVec.$plus$eq(
+                        new Tuple2<>((String) records.readMember(urlParamsLists.get(urlParams, i), "_1"),
+                                (String) records.readMember(urlParamsLists.get(urlParams, i), "_2"))
+                );
+            }
+            // remove last '&'
+            if (canonicalQueryBuilder.length() > 0) {
+                canonicalQueryBuilder.deleteCharAt(canonicalQueryBuilder.length() - 1);
+            }
+
+            String canonicalQueryString = canonicalQueryBuilder.toString();
+
+            // Create the canonical headers and signed headers.
+            // Header names must be trimmed and lowercase, and sorted in code point order from
+            // low to high. Note that there is a trailing \n.
+            // Note: The request can include any headers; canonical_headers and signed_headers lists
+            // those that you want to be included in the hash of the request. "Host" and "x-amz-date" are always required.
+            StringBuilder canonicalHeadersBuilder = new StringBuilder();
+            StringBuilder signedHeadersBuilder = new StringBuilder();
+            VectorBuilder<Tuple2<String, String>> headersParamsVec = new VectorBuilder<>();
+            for (int i = 0; i < headersLists.size(headers); i++) {
+                canonicalHeadersBuilder
+                        .append((String) records.readMember(urlParamsLists.get(headers, i), "_1"))
+                        .append(":")
+                        .append((String) records.readMember(urlParamsLists.get(headers, i), "_2"))
+                        .append("\n");
+                signedHeadersBuilder.append((String) records.readMember(urlParamsLists.get(headers, i), "_1")).append(";");
+
+                headersParamsVec.$plus$eq(
+                        new Tuple2<>((String) records.readMember(urlParamsLists.get(headers, i), "_1"),
+                                (String) records.readMember(urlParamsLists.get(headers, i), "_2"))
+                );
+            }
+            canonicalHeadersBuilder.append("host").append(":").append(host).append("\n");
+            canonicalHeadersBuilder.append("x-amz-date").append(":").append(amzdate).append("\n");
+            signedHeadersBuilder.append("host").append(";").append("x-amz-date");
+
+            String canonicalHeaders = canonicalHeadersBuilder.toString();
+
+            // List of signed headers: lists the headers in the canonical_headers list, delimited with ";".
+            String signedHeaders = signedHeadersBuilder.toString();
+
+            String payloadHash = toHexString(getSha256Digest().digest(bodyString.getBytes(StandardCharsets.UTF_8)));
+
+            String canonicalRequest = method + "\n" +
+                    path + "\n" +
+                    canonicalQueryString + "\n" +
+                    canonicalHeaders + "\n" +
+                    signedHeaders + "\n" +
+                    payloadHash;
+
+            // Task 2: create string to sign
+            // Match the algorithm to the hashing algorithm you use, either SHA-1 or SHA-256 (recommended).
+
+            String algorithm = "AWS4-HMAC-SHA256";
+            String credentialScope = datestamp + "/" + region + "/" + service + "/" + "aws4_request";
+
+            String stringToSign = algorithm + "\n" +
+                    amzdate + "\n" +
+                    credentialScope + "\n" +
+                    toHexString(getSha256Digest().digest(canonicalRequest.getBytes(StandardCharsets.UTF_8)));
+
+            // Task 3: calculate the signature using amazon java example function.
+            byte[] signingKey = getSignatureKey(secretKey, datestamp, region, service);
+            String signature = toHexString(hmacSHA256(stringToSign, signingKey));
+
+            // Task 4: Finally create request using signing information.
+            String authorizationHeader = algorithm + " " +
+                    "Credential=" + key + "/" + credentialScope + ", " +
+                    "SignedHeaders=" + signedHeaders + ", " +
+                    "Signature=" + signature;
+
+            headersParamsVec.$plus$eq(new Tuple2<>("x-amz-date", amzdate));
+            headersParamsVec.$plus$eq(new Tuple2<>("Authorization", authorizationHeader));
+
+            // host is added automatically
+            Map<LocationSettingKey, LocationSettingValue> map = new HashMap<>();
+            map.$plus(Tuple2.apply(new LocationSettingKey("http-method"), new LocationStringSetting(method)));
+            map.$plus(Tuple2.apply(new LocationSettingKey("http-args"), new LocationKVSetting(urlParamsVec.result())));
+            map.$plus(Tuple2.apply(new LocationSettingKey("http-headers"), new LocationKVSetting(headersParamsVec.result())));
+
+            if (!bodyString.isEmpty()) {
+                map.$plus(Tuple2.apply(new LocationSettingKey("http-body-string"), new LocationStringSetting(bodyString)));
+            }
+
+            String url = "https://" + host + "/" + path.replaceAll("^/+", "");
+
+            return new LocationObject(url, map);
+        } catch (UnsupportedMessageException | UnknownIdentifierException e) {
+            throw new RawTruffleInternalErrorException(e);
+        }
+
     }
 }
