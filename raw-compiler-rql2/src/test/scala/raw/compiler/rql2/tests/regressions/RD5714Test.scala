b0189e138e3ceb116f9fbe7cd70a18d30b38aac7
Benjamin Gaidioz <ben@raw-labs.com>
2023-08-03 10:13:36 +0200

    Added duplicated field tests to JSON output

--- raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/regressions/RD5714Test.scala
+++ raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/regressions/RD5714Test.scala
@@ -23,8 +23,12 @@ trait RD5714Test extends CompilerTestContext {
     |    colB = [{id: 2, firstName: "john"}],
     |    join = List.Join(colA, colB, i -> i.name == i.firstName)
     |in Json.Print(join)""".stripMargin) { it =>
-    assume(language != "rql2-truffle") // rql2-truffle fixes duplicated fields as part of RD-9079
-    it should evaluateTo(""" "[{\"id\":1,\"name\":\"john\",\"id_1\":2,\"firstName\":\"john\"}]" """.stripMargin)
+    if (language == "rql2-truffle") {
+      // rql2-truffle fixes duplicated fields as part of RD-9079
+      it should evaluateTo(""" "[{\"id\":1,\"name\":\"john\",\"id\":2,\"firstName\":\"john\"}]" """.stripMargin)
+    } else {
+      it should evaluateTo(""" "[{\"id\":1,\"name\":\"john\",\"id_1\":2,\"firstName\":\"john\"}]" """.stripMargin)
+    }
   }
 
   test("""let colA = [{id: 1, name: "john"}],
@@ -34,12 +38,18 @@ trait RD5714Test extends CompilerTestContext {
     val path = Files.createTempFile("query", "result")
     try {
       it should saveTo(path)
-      assume(language != "rql2-truffle") // rql2-truffle fixes duplicated fields as part of RD-9079
       val source = Source.fromFile(path.toFile)
       val s =
-        try { source.getLines().mkString("\n") }
-        finally { source.close() }
-      assert(s == """[{"id":1,"name":"john","id_1":2,"firstName":"john"}]""")
+        try {
+          source.getLines().mkString("\n")
+        } finally {
+          source.close()
+        }
+      if (language == "rql2-truffle") {
+        assert(s == """[{"id":1,"name":"john","id":2,"firstName":"john"}]""")
+      } else {
+        assert(s == """[{"id":1,"name":"john","id_1":2,"firstName":"john"}]""")
+      }
     } finally {
       Files.delete(path)
     }
