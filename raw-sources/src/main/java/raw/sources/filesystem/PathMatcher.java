1c75521c0b2c63e1e7110d02c49b4699073fe28e
Benjamin Gaidioz <ben@raw-labs.com>
2023-08-11 12:10:58 +0200

    Badly formatted code that however works with doc generation

--- raw-sources/src/main/java/raw/sources/filesystem/PathMatcher.java
+++ raw-sources/src/main/java/raw/sources/filesystem/PathMatcher.java
@@ -65,8 +65,8 @@ public interface PathMatcher {
    *
    * <p>A simple implementation may return the given full path as-is in case of an actual pattern,
    * and the empty String in case of the pattern not containing any dynamic parts (i.e. the <code>
-   * pattern</code> parameter being a static path that wouldn't qualify as an actual {@link
-   * #isPattern pattern}). A sophisticated implementation will differentiate between the static
+   * pattern</code> parameter being a static path that wouldn't qualify as an actual {@link #isPattern pattern}).
+   * A sophisticated implementation will differentiate between the static
    * parts and the dynamic parts of the given path pattern.
    *
    * @param pattern the path pattern
