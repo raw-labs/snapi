940dab4a95b1e065a462339d36bb8a57921077bf
Cesar <cesar.matos@gmail.com>
2023-08-25 11:40:04 +0200

    Applying Miguel's fix

--- raw-compiler-rql2-truffle/src/main/scala/raw/compiler/rql2/truffle/Rql2TruffleCompiler.scala
+++ raw-compiler-rql2-truffle/src/main/scala/raw/compiler/rql2/truffle/Rql2TruffleCompiler.scala
@@ -85,11 +85,18 @@ class Rql2TruffleCompiler(implicit compilerContext: CompilerContext)
   override protected def doEval(
       tree: BaseTree[SourceNode, SourceProgram, Exp]
   )(implicit programContext: raw.compiler.base.ProgramContext): Value = {
-    val entrypoint = doCompile(tree).asInstanceOf[TruffleEntrypoint]
+    val TruffleEntrypoint(context, node, _) = doCompile(tree).asInstanceOf[TruffleEntrypoint]
     RawLanguage.getCurrentContext.setRuntimeContext(programContext.runtimeContext)
-    val target = Truffle.getRuntime.createDirectCallNode(entrypoint.node.getCallTarget)
-    val res = target.call()
-    convertAnyToValue(res, tree.rootType.get)
+    val target = Truffle.getRuntime.createDirectCallNode(node.getCallTarget)
+    try {
+      val res = target.call()
+      convertAnyToValue(res, tree.rootType.get)
+    } finally {
+      // We explicitly created and then entered the context during code emission.
+      // Now we explicitly leave and close the context.
+      context.leave()
+      context.close()
+    }
   }
 
   override def parseAndValidate(source: String, maybeDecl: Option[String])(
@@ -243,6 +250,7 @@ class Rql2TruffleCompiler(implicit compilerContext: CompilerContext)
     // We explicitly create and then enter the context during code emission.
     // This context will be left on close in the TruffleProgramOutputWriter.
     val ctx: Context = Context.newBuilder(RawLanguage.ID).build()
+    logger.debug(s"initializing context: $ctx")
     ctx.initialize(RawLanguage.ID)
     ctx.enter()
 
@@ -500,6 +508,9 @@ class TruffleEmitterImpl(tree: Tree)(implicit programContext: ProgramContext)
       new ProgramExpressionNode(RawLanguage.getCurrentContext.getLanguage, funcFrameDescriptor, functionBody)
     val rootCallTarget = functionRootBody.getCallTarget
     val argNames = fp.ps.map(_.i.idn).toArray
+    logger.debug(
+      "Registering function " + name + " with args " + argNames.mkString(",") + " ctx:" + RawLanguage.getCurrentContext
+    )
     RawLanguage.getCurrentContext.getFunctionRegistry
       .register(name, argNames, rootCallTarget)
   }
@@ -518,6 +529,7 @@ class TruffleEmitterImpl(tree: Tree)(implicit programContext: ProgramContext)
     val functionRootBody =
       new ProgramExpressionNode(RawLanguage.getCurrentContext.getLanguage, funcFrameDescriptor, functionBody)
     val rootCallTarget = functionRootBody.getCallTarget
+    logger.debug("Registering function " + name + " with args " + Array("x").mkString(","))
     val f = RawLanguage.getCurrentContext.getFunctionRegistry
       .register(name, Array("x"), rootCallTarget)
     new ClosureNode(f, Array.empty) // no default values for these lambdas
@@ -557,6 +569,7 @@ class TruffleEmitterImpl(tree: Tree)(implicit programContext: ProgramContext)
         case b: MethodEntity =>
           val funcName = getFuncIdn(b)
           val registry = RawLanguage.getCurrentContext.getFunctionRegistry
+          logger.debug(s"getting function $funcName from ctx: ${RawLanguage.getCurrentContext}")
           val function = registry.getFunction(funcName)
           val defaultArgs = for (p <- b.d.p.ps) yield p.e.map(recurseExp).orNull
           new MethodRefNode(function, defaultArgs.toArray)
