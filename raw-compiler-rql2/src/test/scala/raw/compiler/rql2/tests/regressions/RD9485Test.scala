e059a86e4359db78a3e8c6c8af9cda451de5f81c
Cesar <cesar.matos@gmail.com>
2023-09-01 15:13:14 +0200

    Adding tests

--- raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/regressions/RD9485Test.scala
+++ raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/regressions/RD9485Test.scala
@@ -6,7 +6,7 @@ trait RD9485Test extends CompilerTestContext {
 
   property("raw.inferrer.local.xml.sample-size", "2")
 
-  val xmlFile = tempFile("""<?xml version="1.0" encoding="UTF-8"?>
+  val xmlList = tempFile("""<?xml version="1.0" encoding="UTF-8"?>
     |<top>
     |   <a>1</a>
     |   <a>1.1</a>
@@ -14,7 +14,7 @@ trait RD9485Test extends CompilerTestContext {
     |   <a/>
     |</top>""".stripMargin)
 
-  test(s"""Xml.InferAndRead("file://$xmlFile")""") { it =>
+  test(s"""Xml.InferAndRead("file://$xmlList")""") { it =>
     it should evaluateTo("""{
       |  a: [
       |      1.0,
@@ -24,6 +24,71 @@ trait RD9485Test extends CompilerTestContext {
       |  ]
       |}
       |""".stripMargin)
+  }
+
+  test(s"""Xml.InferAndRead("file://$xmlList", preferNulls=false)""") { it =>
+    it should evaluateTo("""{
+      |  a: [
+      |      1.0,
+      |      1.1,
+      |      Error.Build("failed to parse XML (line 5 column 12): cannot cast 'Hello' to double"),
+      |      Error.Build("failed to parse XML (line 6 column 4): cannot cast '' to double")
+      |  ]
+      |}
+      |""".stripMargin)
+  }
+
+  val xmlRecords = tempFile("""<?xml version="1.0" encoding="UTF-8"?>
+    |<top>
+    |   <a arg1="1" arg2="2">
+    |       <b>3</b>
+    |       <c>Hello</c>
+    |   </a>
+    |   <a arg1="1.1" arg2="2.2">
+    |       <b>3.3</b>
+    |       <c>World!</c>
+    |   </a>
+    |   <a arg1="Hello" arg2="World!">
+    |       <b>Hello</b>
+    |       <c>Again!</c>
+    |   </a>
+    |   <a arg1="where is the other one?"></a>
+    |</top>""".stripMargin)
+
+  test(s"""Xml.InferAndRead("file://$xmlRecords")""") { it =>
+    it should evaluateTo("""{
+      |  a: [
+      |     {
+      |         `@arg1`: 1.0,
+      |         `@arg2`: 2.0,
+      |         b: 3.0,
+      |         c: "Hello"
+      |     },
+      |     {
+      |         `@arg1`: 1.1,
+      |         `@arg2`: 2.2,
+      |         b: 3.3,
+      |         c: "World!"
+      |     },
+      |     {
+      |         `@arg1`: Error.Build("failed to parse XML (line 11 column 4): cannot cast 'Hello' to double"),
+      |         `@arg2`: Error.Build("failed to parse XML (line 11 column 4): cannot cast 'World!' to double"),
+      |         b: Error.Build("failed to parse XML (line 12 column 16): cannot cast 'Hello' to double"),
+      |         c: "Again!"
+      |     },
+      |     {
+      |         `@arg1`: Error.Build("failed to parse XML (line 15 column 4): cannot cast 'where is the other one?' to double"),
+      |         `@arg2`: null,
+      |         b: null,
+      |         c: null
+      |     }
+      |  ]
+      |}
+      |""".stripMargin)
+  }
+
+  test(s"""Xml.InferAndRead("file://$xmlRecords", preferNulls=false)""") { it =>
+    it should runErrorAs("""failed to parse XML""".stripMargin)
   }
 
 }
