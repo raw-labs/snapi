c9c808da8b4b52359fc1db5a07e669e348803d99
Cesar <cesar.matos@gmail.com>
2023-09-05 16:10:38 +0200

    Changing the header so that the CI accepts it

--- raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/regressions/RD9485Test.scala
+++ raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/regressions/RD9485Test.scala
@@ -9,6 +9,7 @@
  * by the Apache License, Version 2.0, included in the file
  * licenses/APL.txt.
  */
+
 package raw.compiler.rql2.tests.regressions
 
 import raw.compiler.rql2.tests.CompilerTestContext
