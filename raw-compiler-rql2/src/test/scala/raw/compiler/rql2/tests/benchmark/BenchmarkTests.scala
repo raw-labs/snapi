89c89cf3dd60978389adeae438cf48af5bf9a64f
alexzerntev <alexzerntev@gmail.com>
2023-09-06 18:16:49 +0300

    Fixed more graph too big errors

--- raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/benchmark/BenchmarkTests.scala
+++ raw-compiler-rql2/src/test/scala/raw/compiler/rql2/tests/benchmark/BenchmarkTests.scala
@@ -24,6 +24,20 @@ trait BenchmarkTests extends CompilerTestContext {
   //  test("""Math.Power(1,1)""")(it => it should run)
   //  test("""{a: {a: {a: {a: {a: {a: {a: {a: {a: {a: {a: {a: 1, b: 2}, b: 2}, b: 2}, b: 2}, b: 2}}}}}}}}""")(it => it should run)
 
+
+//  test("bug-test") { _ =>
+//    executeQuery(
+//      """main(url: string) =
+//        |  let gpxType = type record(
+//        |        trk: record(
+//        |            trkseg: record(
+//        |                trkpt: collection(record(`@lat`: double, `@lon`: double))))),
+//        |      gpx = Xml.Read(url, type gpxType)
+//        |  in gpx
+//        |
+//        |main("file:/home/ld/Downloads/Route Touristique du Champagne Montagne de Reims.gpx")""".stripMargin)
+//  }
+
 //  test("bug-test") { _ =>
 //    executeQuery("""main(url: string) =
 //      |  let gpxType = type record(
@@ -62,7 +76,7 @@ trait BenchmarkTests extends CompilerTestContext {
 //      |        )
 //      |  in map
 //      |
-//      |main("/home/ld/Downloads/Route Touristique du Champagne Montagne de Reims.gpx")""".stripMargin)
+//      |main("file:/home/ld/Downloads/Route Touristique du Champagne Montagne de Reims.gpx")""".stripMargin)
 //  }
 
 //  test("bug-test") { _ =>
