cd97be17b5b9e3f502c4db9e8c71a1f3bfb9477d
Benjamin Gaidioz <ben@raw-labs.com>
2023-09-15 15:32:25 +0200

    Fixing Collection nodes to not pass materialized frames around (#200)

--- raw-runtime-rql2-truffle/src/main/java/raw/runtime/truffle/runtime/iterable/sources/ExpressionCollection.java
+++ raw-runtime-rql2-truffle/src/main/java/raw/runtime/truffle/runtime/iterable/sources/ExpressionCollection.java
@@ -12,11 +12,8 @@
 
 package raw.runtime.truffle.runtime.iterable.sources;
 
-import com.oracle.truffle.api.frame.MaterializedFrame;
-import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import raw.runtime.truffle.ExpressionNode;
 import raw.runtime.truffle.runtime.generator.collection.CollectionAbstractGenerator;
 import raw.runtime.truffle.runtime.generator.collection.compute_next.sources.ExpressionComputeNext;
 import raw.runtime.truffle.runtime.iterable.IterableLibrary;
@@ -37,13 +34,10 @@ so then I have a generator object
 @ExportLibrary(IterableLibrary.class)
 public final class ExpressionCollection {
 
-  final ExpressionNode[] exps;
+  private final Object[] values;
 
-  final MaterializedFrame frame;
-
-  public ExpressionCollection(ExpressionNode[] exps, VirtualFrame frame) {
-    this.exps = exps;
-    this.frame = frame.materialize();
+  public ExpressionCollection(Object[] values) {
+    this.values = values;
   }
 
   @ExportMessage
@@ -53,49 +47,6 @@ public final class ExpressionCollection {
 
   @ExportMessage
   Object getGenerator() {
-    return new CollectionAbstractGenerator(new ExpressionComputeNext(exps, frame));
+    return new CollectionAbstractGenerator(new ExpressionComputeNext(values));
   }
-
-  //    @ExportMessage
-  //    static class GetGenerator {
-  //
-  //        @Specialization
-  //        static Object doGetGenerator(Object receiver,
-  //                                     @CachedLibrary("receiver") IterableLibrary iterables) {
-  //            Object generator = iterables.getGenerator(receiver);
-  //
-  //            new FilterGenerator(generator);
-  ////            // TODO (msb): Where to place this!!!
-  ////             GeneratorLibrary library = GeneratorLibrary.getFactory().create()
-  // .createDispatched(1);
-  ////
-  ////             library.init(generator);
-  //// IntMap y = new IntMap();
-  ////
-  ////             wait.. or do I create here a ...
-  ////            FilterGenerator, passing the generator object I just got as the parent?
-  ////
-  //// yes. create a FilterGenerator class. New instance.
-  ////            that receives the input from above.
-  ////
-  ////
-  //        }
-  ////        @Specialization(guards = "range.index >= range.length")
-  ////        static Object doSequenceEnd(RangeGenerator range) {
-  ////            throw new BreakException();
-  ////        }
-  ////
-  ////        @Specialization(guards = {"range.index < range.length",
-  ////            "range.start == cachedStart"}, limit = "1")
-  ////        static Object doSequenceCached(RangeGenerator range,
-  ////                                       @Cached("range.start") int cachedStart) {
-  ////            return cachedStart + range.index++;
-  ////        }
-  ////
-  ////        @Specialization(replaces = "doSequenceCached")
-  ////        static Object doSequenceUncached(RangeGenerator range) {
-  ////            return doSequenceCached(range, range.start);
-  ////        }
-  //    }
-
 }
