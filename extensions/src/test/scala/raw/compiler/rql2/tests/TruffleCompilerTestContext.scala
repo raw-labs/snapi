8cab418b26243924344efaa4a0016fe5125464f5
Miguel Branco <miguel@raw-labs.com>
2023-09-22 23:02:02 +0200

    Fixes.

--- extensions/src/test/scala/raw/compiler/rql2/tests/TruffleCompilerTestContext.scala
+++ extensions/src/test/scala/raw/compiler/rql2/tests/TruffleCompilerTestContext.scala
@@ -12,8 +12,9 @@
 
 package raw.compiler.rql2.tests
 
-import com.oracle.truffle.api.Truffle
 import org.graalvm.polyglot.Context
+import raw.compiler.base.source.Type
+import raw.compiler.{LSPRequest, LSPResponse}
 import raw.compiler.rql2.Rql2OutputTestContext
 import raw.runtime.ParamValue
 import raw.runtime.truffle.RawLanguage
@@ -23,6 +24,22 @@ import java.nio.file.Path
 class TruffleCompilerTestContext extends CompilerTestContext with Rql2OutputTestContext {
   override def language: String = "rql2-truffle"
 
+  override def tryToType(s: String): Either[Seq[String], Type] = {
+    withTruffleContext(() => super.tryToType(s))
+  }
+
+  override def doLsp(request: LSPRequest): LSPResponse = {
+    withTruffleContext(() => super.doLsp(request))
+  }
+
+  override def doValidate(
+      query: String,
+      options: Map[String, String],
+      scopes: Set[String]
+  ): Either[String, Option[String]] = {
+    withTruffleContext(() => super.doValidate(query, options, scopes))
+  }
+
   override def doExecute(
       query: String,
       maybeDecl: Option[String],
@@ -31,11 +48,29 @@ class TruffleCompilerTestContext extends CompilerTestContext with Rql2OutputTest
       options: Map[String, String],
       scopes: Set[String]
   ): Either[String, Path] = {
+    withTruffleContext(() => super.doExecute(query, maybeDecl, maybeArgs, savePath, options, scopes))
+  }
+
+  override def callDecl(code: String, decl: String, args: Seq[(String, ParamValue)]): Either[String, Any] = {
+    withTruffleContext(() => super.callDecl(code, decl, args))
+  }
+
+  override def fastExecute(
+      query: String,
+      maybeDecl: Option[String],
+      savePath: Option[Path],
+      options: Map[String, String],
+      scopes: Set[String]
+  ): Either[String, Path] = {
+    withTruffleContext(() => super.fastExecute(query, maybeDecl, savePath, options, scopes))
+  }
+
+  private def withTruffleContext[T](f: () => T): T = {
     val ctx: Context = Context.newBuilder(RawLanguage.ID).build()
     ctx.initialize(RawLanguage.ID)
     ctx.enter()
     try {
-      super.doExecute(query, maybeDecl, maybeArgs, savePath, options, scopes)
+      f()
     } finally {
       ctx.leave()
       ctx.close()
