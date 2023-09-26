252762646f631e320de905618be6fd8063205e35
yann <yann@raw-labs.com>
2023-09-27 00:32:35 +0200

    fixup! build script logic + dedicated ci scripts

--- deps/kiama/build.sh
+++ deps/kiama/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 . ~/.sdkman/bin/sdkman-init.sh
+# reset shell env for later stages
+[ "$CI" == "true" ] && {INITAL_PATH=$PATH; unset JAVA_OPTS; }
 sdk install java 17.0.5-amzn
 sdk use java 17.0.5-amzn
 sbt "project core" publishLocal
+[ "$CI" == "true" ] && {PATH=$INITAL_PATH; unset JAVA_HOME; }
