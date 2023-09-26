c15d7fef5bb42f5501d6a77532ceb36eed672274
yann <yann@raw-labs.com>
2023-09-27 00:52:24 +0200

    fixup! fixup! build script logic + dedicated ci scripts

--- deps/scala-logging/build.sh
+++ deps/scala-logging/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
+
 . ~/.sdkman/bin/sdkman-init.sh
 # reset shell env for later stages
-[ "$CI" == "true" ] && {INITAL_PATH=$PATH; unset JAVA_OPTS; }
+[ "$CI" == "true" ] && { INITAL_PATH=$PATH; unset JAVA_OPTS; }
+
 sdk install java 17.0.5-amzn
 sdk use java 17.0.5-amzn
+
+cd "$SCRIPT_HOME"
 sbt publishLocal
-[ "$CI" == "true" ] && {PATH=$INITAL_PATH; unset JAVA_HOME; }
+[ "$CI" == "true" ] && { PATH=$INITAL_PATH; unset JAVA_HOME; }
--- extensions/build.sh
+++ extensions/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
+
 . ~/.sdkman/bin/sdkman-init.sh
 sdk install java 21-graalce
 sdk use java 21-graalce
+
+cd "$SCRIPT_HOME"
 sbt publishLocal
