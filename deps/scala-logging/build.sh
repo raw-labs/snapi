7210472a63c4976298fddc4dd5e0aab372e26f34
yann <yann@raw-labs.com>
2023-09-27 18:27:09 +0200

    fixup! Merge branch 'graal-21-modularize-v2' into graal-21-ci

--- deps/scala-logging/build.sh
+++ deps/scala-logging/build.sh
@@ -5,7 +5,7 @@ SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
 # reset shell env for later stages
 [ "$JAVA_OPTS" == "-XX:+UseG1GC" ] && { unset JAVA_OPTS; }
 
-sdk install java 17.0.5-amzn
+n | sdk install java 17.0.5-amzn || true
 sdk use java 17.0.5-amzn
 
 cd "$SCRIPT_HOME"
