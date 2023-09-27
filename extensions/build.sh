03be4d19ae448b0b65c091d41d563f0a106a02ea
yann <yann@raw-labs.com>
2023-09-27 19:07:45 +0200

    fixup! fixup! fixup! fixup! fixup! javafmt language

--- extensions/build.sh
+++ extensions/build.sh
@@ -2,7 +2,8 @@
 SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
 [ "$CI" == "true" ] && { export HOME=/home/sbtuser; }
 . ~/.sdkman/bin/sdkman-init.sh
-sdk install java 21-graalce
+
+yes | sdk install java 21-graalce || true
 sdk use java 21-graalce
 
 cd "$SCRIPT_HOME"
