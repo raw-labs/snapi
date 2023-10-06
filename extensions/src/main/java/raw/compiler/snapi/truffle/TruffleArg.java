cde25c79956dfdb84f408f9ce7b991523d3a0a53
Alex Zerntev <alex.zerntev@raw-labs.com>
2023-10-06 13:20:44 +0300

    added missed classes

---
+++ extensions/src/main/java/raw/compiler/snapi/truffle/TruffleArg.java
@@ -0,0 +1,40 @@
+///*
+// * Copyright 2023 RAW Labs S.A.
+// *
+// * Use of this software is governed by the Business Source License
+// * included in the file licenses/BSL.txt.
+// *
+// * As of the Change Date specified in that file, in accordance with
+// * the Business Source License, use of this software will be governed
+// * by the Apache License, Version 2.0, included in the file
+// * licenses/APL.txt.
+// */
+//
+//package raw.compiler.snapi.truffle;
+//
+//import raw.compiler.base.source.Type;
+//import raw.runtime.truffle.ExpressionNode;
+//
+//public class TruffleArg {
+//    private final ExpressionNode exprNode;
+//    private final Type type;
+//    private final String identifier;
+//
+//    public TruffleArg(ExpressionNode e, Type t, String identifier) {
+//        this.exprNode = e;
+//        this.type = t;
+//        this.identifier = identifier;
+//    }
+//
+//    public ExpressionNode getExprNode() {
+//        return exprNode;
+//    }
+//
+//    public Type getType() {
+//        return type;
+//    }
+//
+//    public String getIdentifier() {
+//        return identifier;
+//    }
+//}
