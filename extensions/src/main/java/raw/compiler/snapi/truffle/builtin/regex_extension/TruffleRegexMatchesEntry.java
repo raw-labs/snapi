80f2d37bc4ca5ef7b5b5940758beeb03cec7a4ab
Benjamin Gaidioz <ben@raw-labs.com>
2023-10-09 09:33:51 +0200

    Regex

--- extensions/src/main/java/raw/compiler/snapi/truffle/builtin/regex_extension/TruffleRegexMatchesEntry.java
+++ extensions/src/main/java/raw/compiler/snapi/truffle/builtin/regex_extension/TruffleRegexMatchesEntry.java
@@ -1,15 +1,13 @@
 package raw.compiler.snapi.truffle.builtin.regex_extension;
 
+import java.util.List;
 import raw.compiler.base.source.Type;
 import raw.compiler.rql2.builtin.RegexMatchesEntry;
 import raw.compiler.snapi.truffle.TruffleArg;
-import raw.runtime.truffle.RawLanguage;
-import raw.runtime.truffle.ExpressionNode;
 import raw.compiler.snapi.truffle.TruffleEntryExtension;
-import java.util.List;
-
+import raw.runtime.truffle.ExpressionNode;
+import raw.runtime.truffle.RawLanguage;
 import raw.runtime.truffle.ast.expressions.builtin.regex_package.RegexMatchesNodeGen;
-import raw.runtime.truffle.runtime.exceptions.RawTruffleInternalErrorException;
 
 public class TruffleRegexMatchesEntry extends RegexMatchesEntry implements TruffleEntryExtension {
   public ExpressionNode toTruffle(Type type, List<TruffleArg> args, RawLanguage rawLanguage) {
--- extensions/src/main/java/raw/compiler/snapi/truffle/builtin/regex_extension/TruffleRegexReplaceEntry.java
+++ extensions/src/main/java/raw/compiler/snapi/truffle/builtin/regex_extension/TruffleRegexReplaceEntry.java
@@ -1,18 +1,17 @@
 package raw.compiler.snapi.truffle.builtin.regex_extension;
 
+import java.util.List;
 import raw.compiler.base.source.Type;
 import raw.compiler.rql2.builtin.RegexReplaceEntry;
 import raw.compiler.snapi.truffle.TruffleArg;
-import raw.runtime.truffle.RawLanguage;
-import raw.runtime.truffle.ExpressionNode;
 import raw.compiler.snapi.truffle.TruffleEntryExtension;
-import java.util.List;
-
+import raw.runtime.truffle.ExpressionNode;
+import raw.runtime.truffle.RawLanguage;
 import raw.runtime.truffle.ast.expressions.builtin.regex_package.RegexReplaceNodeGen;
-import raw.runtime.truffle.runtime.exceptions.RawTruffleInternalErrorException;
 
 public class TruffleRegexReplaceEntry extends RegexReplaceEntry implements TruffleEntryExtension {
   public ExpressionNode toTruffle(Type type, List<TruffleArg> args, RawLanguage rawLanguage) {
-    return RegexReplaceNodeGen.create(args.get(0).getExprNode(), args.get(1).getExprNode(), args.get(2).getExprNode());
+    return RegexReplaceNodeGen.create(
+        args.get(0).getExprNode(), args.get(1).getExprNode(), args.get(2).getExprNode());
   }
 }
