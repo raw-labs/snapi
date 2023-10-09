0395541900ee03a98f01c6aacde9dbeb2dcbdb1e
Benjamin Gaidioz <ben@raw-labs.com>
2023-10-09 13:58:19 +0200

    fixup! headers

--- extensions/src/main/java/raw/compiler/snapi/truffle/builtin/long_extension/TruffleLongRangeEntry.java
+++ extensions/src/main/java/raw/compiler/snapi/truffle/builtin/long_extension/TruffleLongRangeEntry.java
@@ -15,6 +15,7 @@ package raw.compiler.snapi.truffle.builtin.long_extension;
 import java.util.List;
 import raw.compiler.base.source.Type;
 import raw.compiler.rql2.builtin.LocationLlEntry;
+import raw.compiler.rql2.builtin.LongRangeEntry;
 import raw.compiler.snapi.truffle.TruffleArg;
 import raw.compiler.snapi.truffle.TruffleEntryExtension;
 import raw.runtime.truffle.ExpressionNode;
@@ -22,7 +23,7 @@ import raw.runtime.truffle.RawLanguage;
 import raw.runtime.truffle.ast.expressions.builtin.numeric.long_package.LongRangeNodeGen;
 import raw.runtime.truffle.ast.expressions.literals.LongNode;
 
-public class TruffleLongRangeEntry extends LocationLlEntry implements TruffleEntryExtension {
+public class TruffleLongRangeEntry extends LongRangeEntry implements TruffleEntryExtension {
   @Override
   public ExpressionNode toTruffle(Type type, List<TruffleArg> args, RawLanguage rawLanguage) {
     ExpressionNode start = args.get(0).getExprNode();
