efb94050132db27bb02e3ae05914ba89d1f1c300
alexzerntev <alexzerntev@gmail.com>
2023-10-13 11:06:33 +0300

    format and headers

--- language/src/main/java/raw/compiler/snapi/truffle/builtin/kryo_extension/TruffleKryoDecodeEntry.java
+++ language/src/main/java/raw/compiler/snapi/truffle/builtin/kryo_extension/TruffleKryoDecodeEntry.java
@@ -1,5 +1,18 @@
+/*
+ * Copyright 2023 RAW Labs S.A.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0, included in the file
+ * licenses/APL.txt.
+ */
+
 package raw.compiler.snapi.truffle.builtin.kryo_extension;
 
+import java.util.List;
 import raw.compiler.base.source.Type;
 import raw.compiler.rql2.builtin.KryoDecodeEntry;
 import raw.compiler.rql2.source.ExpType;
@@ -10,12 +23,10 @@ import raw.runtime.truffle.ExpressionNode;
 import raw.runtime.truffle.RawLanguage;
 import raw.runtime.truffle.ast.io.kryo.KryoFromNode;
 
-import java.util.List;
-
 public class TruffleKryoDecodeEntry extends KryoDecodeEntry implements TruffleEntryExtension {
-    @Override
-    public ExpressionNode toTruffle(Type type, List<TruffleArg> args, RawLanguage rawLanguage) {
-        ExpType exprType = (ExpType) args.get(1).type();
-        return new KryoFromNode(args.get(0).exprNode(), (Rql2TypeWithProperties) exprType.t());
-    }
+  @Override
+  public ExpressionNode toTruffle(Type type, List<TruffleArg> args, RawLanguage rawLanguage) {
+    ExpType exprType = (ExpType) args.get(1).type();
+    return new KryoFromNode(args.get(0).exprNode(), (Rql2TypeWithProperties) exprType.t());
+  }
 }
--- language/src/main/java/raw/compiler/snapi/truffle/builtin/test_extension/TruffleValueArg.java
+++ language/src/main/java/raw/compiler/snapi/truffle/builtin/test_extension/TruffleValueArg.java
@@ -1,5 +1,18 @@
+/*
+ * Copyright 2023 RAW Labs S.A.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0, included in the file
+ * licenses/APL.txt.
+ */
+
 package raw.compiler.snapi.truffle.builtin.test_extension;
 
+import java.util.List;
 import raw.compiler.base.source.Type;
 import raw.compiler.snapi.truffle.TruffleArg;
 import raw.compiler.snapi.truffle.TruffleEntryExtension;
@@ -7,8 +20,6 @@ import raw.runtime.truffle.ExpressionNode;
 import raw.runtime.truffle.RawLanguage;
 import raw.runtime.truffle.ast.expressions.literals.StringNode;
 import raw.runtime.truffle.ast.expressions.record.RecordBuildNode;
-
-import java.util.List;
 
 public interface TruffleValueArg extends TruffleEntryExtension {
   @Override
