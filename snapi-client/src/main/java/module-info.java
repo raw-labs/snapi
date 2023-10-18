184462f00389adf90d9e4a9fada8295e38ec3625
Miguel Branco <miguel@raw-labs.com>
2023-10-18 11:50:51 +0200

    Fix formatting.

--- snapi-client/src/main/java/module-info.java
+++ snapi-client/src/main/java/module-info.java
@@ -11,17 +11,16 @@
  */
 
 module raw.snapi.client {
-    requires scala.library;
-    requires org.slf4j;
-    requires org.graalvm.polyglot;
-//    requires org.graalvm.truffle;
+  requires scala.library;
+  requires org.slf4j;
+  requires org.graalvm.polyglot;
+  //    requires org.graalvm.truffle;
 
-    requires raw.utils;
-    requires raw.client;
-    requires raw.snapi.frontend;
-    requires raw.snapi.truffle;
+  requires raw.utils;
+  requires raw.client;
+  requires raw.snapi.frontend;
+  requires raw.snapi.truffle;
 
-    provides raw.client.api.CompilerServiceBuilder with
-            raw.client.rql2.truffle.Rql2TruffleCompilerServiceBuilder;
-
+  provides raw.client.api.CompilerServiceBuilder with
+      raw.client.rql2.truffle.Rql2TruffleCompilerServiceBuilder;
 }
