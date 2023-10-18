8ead77e5e19be71d302228d9eb249206026cda25
Miguel Branco <miguel@raw-labs.com>
2023-10-18 16:24:07 +0200

    WIP

--- snapi-client/src/test/scala/raw/creds/api/CredentialsTestContext.scala
+++ snapi-client/src/test/scala/raw/creds/api/CredentialsTestContext.scala
@@ -13,14 +13,41 @@
 package raw.creds.api
 
 import org.scalatest.BeforeAndAfterAll
-import raw.utils.{RawTestSuite, SettingsTestContext}
+import raw.utils.{withSuppressNonFatalException, AuthenticatedUser, RawTestSuite, SettingsTestContext}
 
 import java.time.temporal.ChronoUnit
+import scala.collection.mutable
 
 trait CredentialsTestContext extends BeforeAndAfterAll {
   this: RawTestSuite with SettingsTestContext =>
 
   private var instance: CredentialsService = _
+
+  private val newHttpCreds = new mutable.ArrayBuffer[(AuthenticatedUser, (String, NewHttpCredential))]()
+  private val s3Buckets = new mutable.HashSet[(AuthenticatedUser, S3Bucket)]()
+  private val rdbmsServers = new mutable.HashMap[(AuthenticatedUser, String), RelationalDatabaseCredential]()
+  private val dropboxTokens = new mutable.HashMap[AuthenticatedUser, DropboxToken]()
+  private val secrets = new mutable.HashSet[(AuthenticatedUser, Secret)]()
+
+  def rdbms(user: AuthenticatedUser, name: String, db: RelationalDatabaseCredential): Unit = {
+    assert(rdbmsServers.put((user, name), db).isEmpty, "Reusing database name with different server")
+  }
+
+  def s3Bucket(user: AuthenticatedUser, s3Bucket: S3Bucket): Unit = {
+    s3Buckets.add((user, s3Bucket))
+  }
+
+  def secret(user: AuthenticatedUser, name: String, value: String): Unit = {
+    secrets.add((user, Secret(name, value)))
+  }
+
+  def dropbox(user: AuthenticatedUser, dropboxToken: DropboxToken): Unit = {
+    dropboxTokens.put(user, dropboxToken)
+  }
+
+  def oauth(user: AuthenticatedUser, name: String, token: NewHttpCredential): Unit = {
+    newHttpCreds.append((user, (name, token)))
+  }
 
   def credentials: CredentialsService = instance
 
@@ -29,7 +56,29 @@ trait CredentialsTestContext extends BeforeAndAfterAll {
     CredentialsServiceProvider.set(credentials)
   }
 
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    s3Buckets.foreach { case (user, bucket) => credentials.registerS3Bucket(user, bucket) }
+    rdbmsServers.foreach { case ((user, name), rdbms) => credentials.registerRDBMSServer(user, name, rdbms) }
+    newHttpCreds.foreach { case (user, (name, cred)) => credentials.registerNewHttpCredential(user, name, cred) }
+    dropboxTokens.foreach { case (user, token) => credentials.registerDropboxToken(user, token) }
+    secrets.foreach { case (user, secret) => credentials.registerSecret(user, secret) }
+  }
+
   override def afterAll(): Unit = {
+    s3Buckets.foreach {
+      case (user, bucket) => withSuppressNonFatalException(credentials.unregisterS3Bucket(user, bucket.name))
+    }
+    rdbmsServers.foreach {
+      case ((user, name), _) => withSuppressNonFatalException(credentials.unregisterRDBMSServer(user, name))
+    }
+    newHttpCreds.foreach {
+      case (user, (name, _)) => withSuppressNonFatalException(credentials.unregisterNewHttpCredential(user, name))
+    }
+    dropboxTokens.foreach { case (user, _) => withSuppressNonFatalException(credentials.unregisterDropboxToken(user)) }
+    secrets.foreach {
+      case (user, secret) => withSuppressNonFatalException(credentials.unregisterSecret(user, secret.name))
+    }
     setCredentials(null)
     super.afterAll()
   }
