8931cb1d55a603128ee24b4193151065f5daa5b0
Miguel Branco <miguel@raw-labs.com>
2023-11-08 16:21:31 +0100

    Add missing services file.

--- snapi-truffle/src/main/java/module-info.java
+++ snapi-truffle/src/main/java/module-info.java
@@ -10,6 +10,8 @@
  * licenses/APL.txt.
  */
 
+import raw.compiler.snapi.truffle.builtin.aws_extension.TruffleAwsV4SignedRequestEntry;
+
 module raw.snapi.truffle {
   // Direct dependencies
   requires java.base;
@@ -62,7 +64,7 @@ module raw.snapi.truffle {
   provides com.oracle.truffle.api.provider.TruffleLanguageProvider with
       raw.runtime.truffle.RawLanguageProvider;
   provides raw.compiler.rql2.api.EntryExtension with
-      raw.compiler.snapi.truffle.builtin.aws_extension.AwsV4SignedRequestEntry,
+      TruffleAwsV4SignedRequestEntry,
       raw.compiler.snapi.truffle.builtin.byte_extension.TruffleByteFromEntry,
       raw.compiler.snapi.truffle.builtin.collection_extension.TruffleEmptyCollectionEntry,
       raw.compiler.snapi.truffle.builtin.collection_extension.TruffleBuildCollectionEntry,
