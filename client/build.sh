cf87bd8265054fa237e1f6c6270a9e6821d803c9
yann <yann@raw-labs.com>
2023-12-18 18:36:06 +0100

    fix(build): better sdk java install

--- client/build.sh
+++ client/build.sh
@@ -3,7 +3,7 @@ SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
 [ "$CI" == "true" ] && { export HOME=/home/sbtuser; }
 . ~/.sdkman/bin/sdkman-init.sh
 
-yes | sdk install java 21-graalce || true
+yes | sdk install java 21.0.1-graalce || true
 sdk use java 21-graalce
 
 cd "$SCRIPT_HOME"
