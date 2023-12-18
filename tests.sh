a2b044824a62f9cc705960e532428d103c4d4515
Yann Bouzonie <yann@raw-labs.com>
2023-12-18 11:23:28 +0100

    update(ci)(cd): use latest raw-scala-runner for CI (#304)

--- tests.sh
+++ tests.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -exu
+#!/bin/bash -ex
 SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
+[ "$CI" == "true" ] && { export HOME=/home/sbtuser; }
+. ~/.sdkman/bin/sdkman-init.sh
 
+yes | sdk install java 21-graalce || true
+sdk use java 21-graalce
 # snapi-frontend
 
 cd "${SCRIPT_HOME}"/snapi-frontend
