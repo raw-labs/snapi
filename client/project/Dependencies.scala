4ad63c15842b4a244a680bd0cc04a500e1a513b4
yann <yann@raw-labs.com>
2023-12-18 14:43:17 +0100

    rollback client deps logic

--- client/project/Dependencies.scala
+++ client/project/Dependencies.scala
@@ -1,36 +1,8 @@
 import sbt._
-import java.nio.file.{Files, Paths}
 
 object Dependencies {
 
-  def findHighestVersionOfRawUtils(): String = {
-    val scalaBinaryVersion: String = scala.util.Properties.versionNumberString.split('.').take(2).mkString(".")
-
-    // Define the path to the Ivy cache for raw-utils
-    val ivyCachePath = Path.userHome / ".ivy2" / "local" / "com.raw-labs" / s"raw-utils_$scalaBinaryVersion"
-
-    // Check if the path exists and list all versions
-    if (ivyCachePath.exists) {
-      ivyCachePath.listFiles()
-        .filter(_.isDirectory)
-        .map(_.name)
-        .sorted
-        .lastOption
-        .getOrElse(throw new RuntimeException("No version found in Ivy cache for raw-utils"))
-    } else {
-      throw new RuntimeException("Ivy cache for raw-utils not found")
-    }
-  }
-
-  val rawUtilsVersion = {
-    val versionFilePath = "../utils/version"
-    if (Files.exists(Paths.get(versionFilePath))) {
-      IO.read(new File(versionFilePath)).trim
-    } else {
-      findHighestVersionOfRawUtils()
-    }
-  }
-
+  val rawUtilsVersion = IO.read(new File("../utils/version")).trim
   val rawUtils = "com.raw-labs" %% "raw-utils" % rawUtilsVersion
 
   val trufflePolyglot = "org.graalvm.polyglot" % "polyglot" % "23.1.0"
