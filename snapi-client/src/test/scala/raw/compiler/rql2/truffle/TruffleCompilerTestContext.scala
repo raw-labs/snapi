421c043fe4878a61f7af8b54d3050e913baead2e
Miguel Branco <miguel@raw-labs.com>
2023-12-18 14:02:08 +0100

    RD-10251 CompilerContext created multiple times. (#295)
    Co-authored-by: Benjamin Gaidioz <ben@raw-labs.com>

--- snapi-client/src/test/scala/raw/compiler/rql2/truffle/TruffleCompilerTestContext.scala
+++ snapi-client/src/test/scala/raw/compiler/rql2/truffle/TruffleCompilerTestContext.scala
@@ -14,8 +14,23 @@ package raw.compiler.rql2.truffle
 
 import raw.compiler.rql2.api.Rql2OutputTestContext
 import raw.compiler.rql2.tests.CompilerTestContext
+import raw.runtime.truffle.RawLanguage
 
 class TruffleCompilerTestContext
     extends CompilerTestContext
     with Rql2OutputTestContext
-    with Rql2TruffleCompilerServiceTestContext
+    with Rql2TruffleCompilerServiceTestContext {
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    // Forcibly drop all language caches before each test suite.
+    RawLanguage.dropCaches()
+  }
+
+  override def afterAll(): Unit = {
+    // Forcibly drop all language caches after each test suite.
+    RawLanguage.dropCaches()
+    super.afterAll()
+  }
+
+}
