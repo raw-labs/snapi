bf67cc4235ed25fbba96ffbada97e78b8f09b792
Alex Zerntev <alex.zerntev@raw-labs.com>
2024-01-11 13:07:54 +0200

    cleanup

--- snapi-truffle/src/main/java/raw/runtime/truffle/ast/expressions/iterable/list/ListTakeNode.java
+++ snapi-truffle/src/main/java/raw/runtime/truffle/ast/expressions/iterable/list/ListTakeNode.java
@@ -25,7 +25,7 @@ import raw.runtime.truffle.runtime.list.*;
 public abstract class ListTakeNode extends ExpressionNode {
 
   @Specialization
-  protected Object doByte(
+  protected Object take(
       Object list, long num, @Cached(inline = true) ListNodes.TakeNode takeNode) {
     return takeNode.execute(this, list, (int) num);
   }
