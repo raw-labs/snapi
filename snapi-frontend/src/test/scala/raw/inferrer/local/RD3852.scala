4e61591f8503a5c7db7190edfcb51a5d6a19d4d6
torcato <cesar.matos@gmail.com>
2024-01-15 14:02:44 +0100

    Adding header to test file

--- snapi-frontend/src/test/scala/raw/inferrer/local/RD3852.scala
+++ snapi-frontend/src/test/scala/raw/inferrer/local/RD3852.scala
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2023 RAW Labs S.A.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file licenses/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0, included in the file
+ * licenses/APL.txt.
+ */
+
 package raw.inferrer.local
 
 import com.typesafe.scalalogging.StrictLogging
@@ -14,7 +26,6 @@ import raw.utils.{RawTestSuite, RawUtils, SettingsTestContext}
 import raw.sources.filesystem.local.LocalPath
 
 class RD3852 extends RawTestSuite with SettingsTestContext with StrictLogging with LocalLocationsTestContext {
-
 
   // CSV is the last in the list in the auto inferrer.
   // So inferring this a csv file means that the other inferrers threw  correctly
