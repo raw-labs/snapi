768877e904863fd823279d26cbae80e59bd3a6d9
Georgios Kiomourtzis <89911727+gkiomour@users.noreply.github.com>
2024-01-30 13:50:59 +0200

    External connector api (#346)
    Co-authored-by: Thanasis Stouraitis <thanasis.stouraitis@raw-labs.com>

--- snapi-frontend/src/main/scala/raw/creds/protocol/ProtocolCredentials.scala
+++ snapi-frontend/src/main/scala/raw/creds/protocol/ProtocolCredentials.scala
@@ -25,15 +25,14 @@ final case class RegisterDropboxCredential(user: AuthenticatedUser, token: Dropb
 final case class GetDropboxCredential(user: AuthenticatedUser)
 final case class UnregisterDropboxCredential(user: AuthenticatedUser)
 
-final case class RegisterSalesforceCredential(
+final case class RegisterExternalConnectorCredential(
     user: AuthenticatedUser,
     name: String,
-    salesforceCredential: SalesforceCredential
+    externalConnectorCredentialProtocol: ExternalConnectorCredential
 )
-final case class GetSalesforceCredential(user: AuthenticatedUser, name: String)
-final case class ListSalesforceCredentials(user: AuthenticatedUser)
-
-final case class UnregisterSalesforceCredential(user: AuthenticatedUser, name: String)
+final case class GetExternalConnectorCredential(user: AuthenticatedUser, name: String)
+final case class ListExternalConnectorCredential(user: AuthenticatedUser)
+final case class UnregisterExternalConnectorCredential(user: AuthenticatedUser, name: String)
 
 final case class RegisterHttpCredential(user: AuthenticatedUser, credential: HttpCredential)
 final case class ListHttpCredentials(user: AuthenticatedUser)
