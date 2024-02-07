dd840a2b409adf6fde7a33ddcb8d33943c4122ad
torcato <cesar.matos@gmail.com>
2024-02-07 17:10:50 +0100

    Changing sql to filter schema  pg_catalog and information_schema

--- sql-client/src/main/scala/raw/client/sql/metadata/WordSearchWithOneItem.scala
+++ sql-client/src/main/scala/raw/client/sql/metadata/WordSearchWithOneItem.scala
@@ -18,13 +18,17 @@ object WordSearchWithOneItem
     extends Completion(
       """SELECT name, type FROM (
         |-- schemas that have a name starting with the identifier
-        |SELECT schema_name AS name, 'schema' AS type FROM information_schema.schemata WHERE starts_with(schema_name, ?)
+        |SELECT schema_name AS name, 'schema' AS type FROM information_schema.schemata
+        |	WHERE starts_with(schema_name, ?) AND schema_name NOT IN ('pg_catalog', 'information_schema')
         |UNION
         |-- tables that have a name starting with the identifier
-        |SELECT table_name AS name, 'table' AS type FROM information_schema.tables WHERE starts_with(table_name, ?)
+        |SELECT table_name AS name, 'table' AS type FROM information_schema.tables
+        |	WHERE starts_with(table_name, ?) AND table_schema NOT IN ('pg_catalog', 'information_schema')
         |UNION
         |-- columns that have a name starting with the identifier
-        |SELECT column_name AS name, data_type AS type FROM information_schema.columns WHERE starts_with(column_name, ?)) T""".stripMargin
+        |SELECT column_name AS name, data_type AS type FROM information_schema.columns
+        |	WHERE starts_with(column_name, ?) AND table_schema NOT IN ('pg_catalog', 'information_schema')
+        |) T""".stripMargin
     ) {
   override protected def setParams(preparedStatement: PreparedStatement, items: Seq[String]): Unit = {
     val item = items.head
