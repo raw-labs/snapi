243b62b3fe3065de010b7bb3281a8dd19cae7724
Yann Bouzonie <yann@raw-labs.com>
2024-02-13 09:13:08 +0100

    refactor(build): less aggressive jvm setup (#341)
    Let's answer no if sdk man is asking to use a new jvm as default

--- sql-client/release.sh
+++ sql-client/release.sh
@@ -3,7 +3,7 @@ SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
 [ "$CI" == "true" ] && { export HOME=/home/sbtuser; }
 . ~/.sdkman/bin/sdkman-init.sh
 
-yes | sdk install java 21.0.1-graalce || true
+yes n | sdk install java 21.0.1-graalce || true
 sdk use java 21.0.1-graalce
 
 cd "$SCRIPT_HOME"
