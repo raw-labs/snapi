ac9af7ea421581befee616ee7d9742b3577e9356
alexzerntev <alexzerntev@gmail.com>
2024-03-04 23:21:14 +0200

    metrics

--- snapi-client/src/test/scala/raw/compiler/rql2/tests/benchmark/BenchmarkTests.scala
+++ snapi-client/src/test/scala/raw/compiler/rql2/tests/benchmark/BenchmarkTests.scala
@@ -22,111 +22,66 @@ trait BenchmarkTests extends CompilerTestContext {
 //  test(
 //    """let
 //      |    lineitemsType = type collection(record(l_orderkey: int, l_payedammount: double)),
-//      |    customerType = type record(
-//      |        customer: collection(record(c_custkey: string, c_name: string, c_address: string, c_phone: string))
-//      |    ),
+//      |    customerType = type record(customer: collection(record(c_custkey: string))),
 //      |    ordersType = type collection(record(o_orderkey: int, o_custkey: int)),
 //      |    lineitems = PostgreSQL.Query(
 //      |        "postgres",
-//      |        "select l_orderkey, (l_extendedprice * (1 - l_discount)) as l_payedammount from tpch10.lineitem",
+//      |        "select l_orderkey, (l_extendedprice * (1 - l_discount)) as l_payedammount from tpch1.lineitem",
 //      |        lineitemsType,
 //      |        host = "localhost",
 //      |        username = "postgres",
 //      |        password = "1234"
 //      |    ),
-//      |    customers = Json.Read("file:///home/ld/workspace/TPCH/10GB/customer.json", customerType), // an object with an array inside
-//      |    orders = Csv.Read("file:///home/ld/workspace/TPCH/10GB/orders.csv", ordersType, delimiter = "\t"),
+//      |    customers = Json.Read("file:///home/ld/workspace/TPCH/1GB/customer.json", customerType), // an object with an array inside
+//      |    orders = Csv.Read("file:///home/ld/workspace/TPCH/1GB/orders.csv", ordersType, delimiter = "\t"),
 //      |    customersOrders = Collection.EquiJoin(customers.customer, orders, (c) -> Int.From(c.c_custkey), (o) -> o.o_custkey),
-//      |    ordersItems = Collection.EquiJoin(orders, lineitems, (o) -> o.o_orderkey, (l) -> l.l_orderkey),
-//      |    customerOrdersItems = Collection.EquiJoin(customersOrders, ordersItems, (co) -> Int.From(co.c_custkey), (oi) -> oi.o_custkey),
-//      |    grouped = Collection.GroupBy(
-//      |        customerOrdersItems,
-//      |        (c) -> c.c_custkey + "|" + c.c_name + "|" + c.c_address + "|" + c.c_phone
+//      |    customerOrdersItems = Collection.EquiJoin(
+//      |        customersOrders,
+//      |        lineitems,
+//      |        (co) -> Int.From(co.o_orderkey),
+//      |        (oi) -> oi.l_orderkey
 //      |    ),
-//      |    result = Collection.Transform(
-//      |        grouped,
-//      |        (g) ->
-//      |            let
-//      |                splited = String.Split(g.key, "|"),
-//      |                name = List.Get(splited, 1),
-//      |                address = List.Get(splited, 2),
-//      |                phone = List.Get(splited, 3)
-//      |            in
-//      |                {name: name, address: address, phone: phone, total_payed: Collection.Sum(g.group.l_payedammount)}
-//      |    )
+//      |    grouped = Collection.GroupBy(customerOrdersItems, (c) -> c.c_custkey),
+//      |    result = Collection.Transform(grouped, (g) -> {id: g, total_payed: Collection.Sum(g.group.l_payedammount)}),
+//      |    finalResult = Collection.Filter(result, (r) -> r.total_payed > 6000000)
 //      |in
-//      |    Collection.Take(ordersItems, 5)""".stripMargin
+//      |    Collection.Count(finalResult)""".stripMargin
 //  )(_ should evaluateTo("[]"))
 
   test("Range Join File with db test") { _ =>
     assume(false, "This test is disabled by default")
-    fastExecute(
-      """let
-        |    lineitemsType = type collection(
-        |        record(
-        |           l_orderkey: int,
-        |           l_partkey: int,
-        |           l_suppkey: int,
-        |           l_linenumber: int,
-        |           l_quantity: double,
-        |           l_extendedprice: double,
-        |           l_discount: double,
-        |           l_tax: double,
-        |           l_returnflag: string,
-        |           l_linestatus: string,
-        |           l_shipdate: date,
-        |           l_commitdate: date,
-        |           l_receiptdate: date,
-        |           l_shipinstruct: string,
-        |           l_shipmode: string,
-        |           l_comment: string,
-        |           l_dummy: string
-        |        )
-        |    ),
-        |    customerType = type record(
-        |        customer: collection(
-        |            record(
-        |                c_custkey: string,
-        |                c_name: string,
-        |                c_address: string,
-        |                c_nationkey: string,
-        |                c_phone: string,
-        |                c_acctbal: string,
-        |                c_mktsegment: string,
-        |                c_comment: string,
-        |                c_dummy: string
-        |            )
-        |        )
-        |    ),
-        |    ordersType = type collection(
-        |        record(
-        |            o_orderkey: int,
-        |            o_custkey: int,
-        |            o_orderstatus: string,
-        |            o_totalprice: double,
-        |            o_orderdate: string,
-        |            o_orderpriority: string,
-        |            o_clerk: string,
-        |            o_shippriority: int,
-        |            o_comment: string,
-        |            o_dummy: string
-        |        )
-        |    ),
-        |    lineitems = PostgreSQL.Read(
-        |        "postgres",
-        |        "tpch10",
-        |        "lineitem",
-        |        lineitemsType,
-        |        host = "localhost",
-        |        username = "postgres",
-        |        password = "1234"
-        |    ),
-        |    customers = Json.Read("file:///home/ld/workspace/TPCH/10GB/customer.json", customerType), // an object with an array inside
-        |    orders = Csv.Read("file:///home/ld/workspace/TPCH/10GB/orders.csv", ordersType, delimiter = "\t")
-        |in
-        |    List.Build(Collection.Count(suppliers), Collection.Count(customers.customer), Collection.Count(orders))""".stripMargin
-    )
+    val prog = """let
+      |    lineitemsType = type collection(record(l_orderkey: int, l_payedammount: double)),
+      |    customerType = type record(customer: collection(record(c_custkey: string))),
+      |    ordersType = type collection(record(o_orderkey: int, o_custkey: int)),
+      |    lineitems = PostgreSQL.Query(
+      |        "postgres",
+      |        "select l_orderkey, (l_extendedprice * (1 - l_discount)) as l_payedammount from tpch1.lineitem",
+      |        lineitemsType,
+      |        host = "localhost:44444",
+      |        username = "postgres",
+      |        password = "1234"
+      |    ),
+      |    customers = Json.Read("file:///home/ld/workspace/TPCH/1GB/customer.json", customerType), // an object with an array inside
+      |    orders = Csv.Read("file:///home/ld/workspace/TPCH/1GB/orders.csv", ordersType, delimiter = "\t"),
+      |    customersOrders = Collection.EquiJoin(customers.customer, orders, (c) -> Int.From(c.c_custkey), (o) -> o.o_custkey),
+      |    customerOrdersItems = Collection.EquiJoin(
+      |        customersOrders,
+      |        lineitems,
+      |        (co) -> Int.From(co.o_orderkey),
+      |        (oi) -> oi.l_orderkey
+      |    ),
+      |    grouped = Collection.GroupBy(customerOrdersItems, (c) -> c.c_custkey),
+      |    result = Collection.Transform(grouped, (g) -> {id: g, total_payed: Collection.Sum(g.group.l_payedammount)}),
+      |    finalResult = Collection.Filter(result, (r) -> r.total_payed > 6000000)
+      |in
+      |    Collection.Count(finalResult)""".stripMargin
 
+//    fastExecute("""let a = "hello" in a """) // some random query
+//    fastExecute("""let a = 2 + 2 in a """) // some random query
+//    fastExecute("""let a = 2/2 in a """) // some random query
+
+    fastExecute(prog)
   }
 
   test("Range Count test") { _ =>
