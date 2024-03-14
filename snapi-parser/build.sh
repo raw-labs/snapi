7dec4f8b41930327aaea45a8433fa6d2a9e7abed
yann <yann@raw-labs.com>
2024-03-14 16:31:59 +0100

    refactor(snapi-parser)(build): build with maven

--- snapi-parser/build.sh
+++ snapi-parser/build.sh
@@ -7,4 +7,6 @@ yes n | sdk install java 21-graalce || true
 sdk use java 21-graalce
 
 cd "$SCRIPT_HOME"
-sbt clean publishLocal
+NEW_VERSION=$(git describe --tags | sed 's/^v//;s/-\([0-9]*\)-g/+\1-/')
+mvn versions:set -DnewVersion=$NEW_VERSION
+mvn clean install
