94d9dff0fe7dec5eae7b179a086b1d9844bc0949
yann <yann@raw-labs.com>
2024-03-26 10:31:18 +0100

    not running inside container for now

--- snapi-parser/release.sh
+++ snapi-parser/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
-[ "$CI" == "true" ] && { export HOME=/home/sbtuser; export MAVEN_CLI_OPTS="-B";}
+[ "$CI" == "true" ] && { export MAVEN_CLI_OPTS="-B";}
 . ~/.sdkman/bin/sdkman-init.sh
 
 yes n | sdk install java 21-graalce || true
@@ -10,5 +10,6 @@ cd "$SCRIPT_HOME"
 
 VERSION=$(git describe --tags | sed 's/^v//;s/-\([0-9]*\)-g/+\1-/')
 export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED"
+mvn clean deploy -Pdev,deploy -Drevision=$VERSION -Dchangelist=
 mvn nexus-staging:release -Pdev,deploy -Drevision=$VERSION -Dchangelist=
 echo "${VERSION}" > version
