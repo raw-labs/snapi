7aff55ce71c70dca27ce6cacac78f608810227e5
yann <yann@raw-labs.com>
2024-03-26 10:31:18 +0100

    not running inside container for now

--- snapi-parser/publish.sh
+++ snapi-parser/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
-[ "$CI" == "true" ] && { export HOME=/home/sbtuser; export MAVEN_CLI_OPTS="-B";}
+[ "$CI" == "true" ] && { export MAVEN_CLI_OPTS="-B";}
 . ~/.sdkman/bin/sdkman-init.sh
 
 yes n | sdk install java 21-graalce || true
@@ -9,5 +9,5 @@ sdk use java 21-graalce
 cd "$SCRIPT_HOME"
 
 VERSION=$(git describe --tags | sed 's/^v//;s/-\([0-9]*\)-g/+\1-/')
-mvn clean deploy -Pdev,deploy -Drevision=$VERSION -Dchangelist=
+mvn clean deploy -Pdev,deploy -Drevision=$VERSION
 echo "${VERSION}" > version
