4bf3e949492eabad37191b18257c21b6d7269c01
Benjamin Gaidioz <ben@raw-labs.com>
2024-04-17 12:47:32 +0200

    Added CSV tests

--- snapi-client/src/test/scala/raw/compiler/rql2/tests/regressions/RD10801Test.scala
+++ snapi-client/src/test/scala/raw/compiler/rql2/tests/regressions/RD10801Test.scala
@@ -24,6 +24,39 @@ trait RD10801Test extends CompilerTestContext {
     _ should runErrorAs("unsupported type")
   )
 
+  private val jsonContent = """[
+    |  { "f1": "", "f2": "", "f3": "", "f4": "", "f5": "", "f6": "", "f7": "" },
+    |  { "f8": "", "f9": "", "dup": "", "f10": "", "f11": "", "f12": "", "f13": "", "f14": "", "f15": "", "dup": "" }
+    |] """.stripMargin
+  private val rd10801Json = tempFile(jsonContent, "json")
+
+  test(s"""Json.InferAndParse($qqq$jsonContent$qqq)""".stripMargin)(_ should runErrorAs("unsupported type"))
+  test(
+    s"""Json.Parse($qqq$jsonContent$qqq, type record(f1: string, f2: string, dup: string, f4: string, dup: string)"""
+  )(_ should runErrorAs("unsupported type"))
+
+  test(snapi"""Json.InferAndRead("$rd10801Json")""")(_ should runErrorAs("unsupported type"))
+  test(snapi"""Json.Read("$rd10801Json", type record(f1: string, f2: string, dup: string, f4: string, dup: string)""")(
+    _ should runErrorAs("unsupported type")
+  )
+
+  private val csvContent = """f1,f2,f3,dup,f5,dup
+    |v1,v2,v3,v4,v5,v6
+    |v1,v2,v3,v4,v5,v6""".stripMargin
+  private val rd10801Csv = tempFile(csvContent, "csv")
+  test(s"""Csv.InferAndParse($qqq$csvContent$qqq)""".stripMargin)(_ should run)
+  test(
+    s"""Csv.Parse($qqq$csvContent$qqq, type collection(record(f1: string, f2: string, f3: string, dup: string, f5: string, dup: string)),
+      |     delimiter = ",")""".stripMargin
+  )(_ should run)
+
+  test(snapi"""Csv.InferAndRead($qqq$rd10801Csv$qqq)""".stripMargin)(_ should run)
+  test(
+    snapi"""Csv.Read($qqq$rd10801Csv$qqq,
+      |type collection(record(f1: string, f2: string, f3: string, dup: string, f5: string, dup: string)),
+      |delimiter = ",")""".stripMargin
+  )(_ should run)
+
   test(
     s"""Xml.Parse($qqq<?xml version="1.0"?><nothing/>$qqq, type record(nothing: collection(record(a: (int) -> float, b: int, c: int))))"""
   )(
@@ -76,16 +109,5 @@ trait RD10801Test extends CompilerTestContext {
   )(
     _ should runErrorAs("unsupported type; duplicate field: a")
   )
-
-  private val rd10801Json = """[
-    |  { "f1": "", "f2": "", "f3": "", "f4": "", "f5": "", "f6": "", "f7": "" },
-    |  { "f8": "", "f9": "", "dup": "", "f10": "", "f11": "", "f12": "", "f13": "", "f14": "", "f15": "", "dup": "" }
-    |] """.stripMargin
-
-  test(s"""Json.InferAndParse($qqq$rd10801Json$qqq)""".stripMargin)(_ should runErrorAs("unsupported type"))
-
-  private val dataset = tempFile(rd10801Json, "json")
-
-  test(snapi"""Json.InferAndRead("$dataset")""")(_ should runErrorAs("unsupported type"))
 
 }
