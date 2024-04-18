a20f8e45da8d2e8f9b3782273dfaeb6206fd8f5c
yann <yann@raw-labs.com>
2024-04-19 00:28:47 +0200

    fixup! fixup! adapt ci publish and last cleanup

--- publishLocal.sh
+++ publishLocal.sh
@@ -3,4 +3,11 @@ SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
 
 ./rebuild.sh
 
+export COURSIER_PROGRESS=false
+[ "$CI" == "true" ] && { export HOME=/home/sbtuser; }
+. ~/.sdkman/bin/sdkman-init.sh
+
+yes n | sdk install java 21.0.1-graalce || true
+sdk use java 21.0.1-graalce
+
 sbt publishLocal
