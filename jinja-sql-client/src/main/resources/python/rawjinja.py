1dceca4e754e55ec7dc17b0033cc892c14014be7
Benjamin Gaidioz <ben@raw-labs.com>
2024-05-03 17:10:57 +0200

    wip

--- jinja-sql-client/src/main/resources/python/rawjinja.py
+++ jinja-sql-client/src/main/resources/python/rawjinja.py
@@ -8,12 +8,35 @@ class RawJinjaException(Exception):
    def message(self):
       return self._message
 
-env = Environment()
+
+class Safe:
+
+    def __init__(self, s):
+        self._value = s
+
+    def value(self):
+        return self._value
+
+def fix(s):
+    print("Fixing: %s" % s)
+    if isinstance(s, Safe):
+        return s.value()
+    else:
+        return "'" + s.replace("'", "''") + "'"
+
+def do_safe(s):
+    return Safe(s)
+
+env = Environment(finalize=fix, autoescape=False)
+env.filters['safe'] = do_safe
+
+def _apply(code, args):
+   template = env.from_string(code)
+   return template.render(args)
 
 def apply(code, args):
-   template = env.from_string(code)
    d = {key: args.get(key) for key in args.keySet()}
-   return template.render(d)
+   return _apply(d)
 
 def validate(code):
   tree = env.parse(code)
