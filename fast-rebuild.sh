235df1570b74b71536b3756302c17de27802d45e
yann <yann@raw-labs.com>
2024-06-12 14:19:47 +0200

    fast rebuild script

---
+++ fast-rebuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+SCRIPT_HOME="$(cd "$(dirname "$0")"; pwd)"
+
+find . -type d -name "target" -exec rm -r {} \; || true
+
+export COURSIER_PROGRESS=false
+[ "$CI" == "true" ] && { export HOME=/home/sbtuser; }
+. ~/.sdkman/bin/sdkman-init.sh
+
+yes n | sdk install java 21.0.1-graalce || true
+sdk use java 21.0.1-graalce
+
+cd "${SCRIPT_HOME}"
+sbt clean compile
