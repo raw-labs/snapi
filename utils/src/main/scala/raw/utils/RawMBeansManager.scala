eee7a2811c3b34aef5074c996f09e2c04990fc79
Miguel Branco <miguel@raw-labs.com>
2024-07-04 18:08:59 +0200

    Fixes.

--- utils/src/main/scala/raw/utils/RawMBeansManager.scala
+++ utils/src/main/scala/raw/utils/RawMBeansManager.scala
@@ -18,7 +18,16 @@ import java.lang.management.ManagementFactory
 import javax.management.ObjectName
 import scala.util.control.NonFatal
 
+/**
+ * Base Trait for MBeans
+ */
 trait RawMBean {
+
+  /**
+   * Get the name of the MBean
+   *
+   * @return the name of the MBean
+   */
   def getMBeanName: String
 }
 
@@ -26,39 +35,65 @@ object RawMBeansManager extends StrictLogging {
 
   private val mbs = ManagementFactory.getPlatformMBeanServer
   private var mbeanMap = Map.empty[String, RawMBean]
+  private val mbeanLock = new Object
 
+  /**
+   * Register an MBean
+   *
+   * @param mbean the MBean to register
+   */
   def registerMBean(mbean: RawMBean): Unit = {
     require(mbean != null, "MBean cannot be null")
     require(mbean.getMBeanName != null, "MBean name cannot be null")
-    if (mbs.isRegistered(new ObjectName(mbean.getMBeanName))) {
-      logger.warn(s"MBean ${mbean.getMBeanName} already registered")
-    } else {
-      val objectName = new ObjectName(mbean.getMBeanName)
-      mbs.registerMBean(mbean, objectName)
-      mbeanMap += (mbean.getMBeanName() -> mbean)
+    mbeanLock.synchronized {
+      if (mbs.isRegistered(new ObjectName(mbean.getMBeanName))) {
+        logger.warn(s"MBean ${mbean.getMBeanName} already registered")
+      } else {
+        val objectName = new ObjectName(mbean.getMBeanName)
+        mbs.registerMBean(mbean, objectName)
+        mbeanMap += (mbean.getMBeanName -> mbean)
+      }
     }
   }
 
+  /**
+   * Unregister an MBean
+   *
+   * @param name the name of the MBean to unregister
+   */
   def unregisterMBean(name: String): Unit = {
-    val objectName = new ObjectName(name)
-    if (mbs.isRegistered(objectName)) {
-      mbs.unregisterMBean(objectName)
-      mbeanMap -= name
-    } else {
-      logger.warn(s"MBean $name is not registered")
-    }
-  }
-
-  def getMBeanCopy[T](name: String): Option[T] = {
-    try {
+    require(name != null, "MBean name cannot be null")
+    mbeanLock.synchronized {
       val objectName = new ObjectName(name)
       if (mbs.isRegistered(objectName)) {
-        Some(mbeanMap(name).asInstanceOf[T])
+        mbs.unregisterMBean(objectName)
+        mbeanMap -= name
       } else {
-        None
+        logger.warn(s"MBean $name is not registered")
       }
-    } catch {
-      case NonFatal(_) => None
+    }
+  }
+
+  /**
+   * Get a copy of an MBean
+   *
+   * @param name the name of the MBean to get
+   * @tparam T the type of the MBean
+   * @return an option containing the MBean if it exists, None otherwise
+   */
+  def getMBeanCopy[T](name: String): Option[T] = {
+    require(name != null, "MBean name cannot be null")
+    mbeanLock.synchronized {
+      try {
+        val objectName = new ObjectName(name)
+        if (mbs.isRegistered(objectName)) {
+          Some(mbeanMap(name).asInstanceOf[T])
+        } else {
+          None
+        }
+      } catch {
+        case NonFatal(_) => None
+      }
     }
   }
 
