47d11cc3fb0a3c1cb49a4a879e61acd223190f9a
Cesar Torcato de Matos <cesar.matos@gmail.com>
2024-08-26 10:07:17 +0200

    RD-12436 Adding examples for snowflake options, RD-12435 Add documentation to Location.String (#478)

--- snapi-frontend/src/main/scala/com/rawlabs/snapi/frontend/snapi/extensions/builtin/SnowflakePackage.scala
+++ snapi-frontend/src/main/scala/com/rawlabs/snapi/frontend/snapi/extensions/builtin/SnowflakePackage.scala
@@ -97,11 +97,12 @@ class SnowflakeInferAndReadEntry extends SugarEntryExtension {
       ParamDoc(
         "options",
         typeDoc = TypeDoc(List("list")),
-        description = """Extra connection options.""".stripMargin,
+        description = """Extra connection options, e.g. `[{"timezone", "UTC"}]`.""".stripMargin,
         isOptional = true
       )
     ),
-    examples = List(ExampleDoc("""Snowflake.InferAndRead("database", "schema", "table")""")),
+    examples =
+      List(ExampleDoc("""Snowflake.InferAndRead("database", "schema", "table", options=[{"timezone", "UTC"}])""")),
     ret = Some(
       ReturnDoc("A table with the data read from the Snowflake table.", retType = Some(TypeDoc(List("collection"))))
     )
@@ -282,7 +283,7 @@ class SnowflakeReadEntry extends SugarEntryExtension {
       ParamDoc(
         "options",
         typeDoc = TypeDoc(List("list")),
-        description = """Extra connection options.""".stripMargin,
+        description = """Extra connection options, e.g. `[{"timezone", "UTC"}]`.""".stripMargin,
         isOptional = true
       )
     ),
@@ -426,11 +427,13 @@ class SnowflakeInferAndQueryEntry extends SugarEntryExtension {
       ParamDoc(
         "options",
         typeDoc = TypeDoc(List("list")),
-        description = """Extra connection options.""".stripMargin,
+        description = """Extra connection options, e.g. `[{"timezone", "UTC"}]`.""".stripMargin,
         isOptional = true
       )
     ),
-    examples = List(ExampleDoc("""Snowflake.InferAndQuery("database", "SELECT * FROM schema.table")""")),
+    examples = List(
+      ExampleDoc("""Snowflake.InferAndQuery("database", "SELECT * FROM schema.table", options=[{"timezone", "UTC"}])""")
+    ),
     ret = Some(
       ReturnDoc("A table with the data read from the Snowflake table.", retType = Some(TypeDoc(List("collection"))))
     )
@@ -601,7 +604,7 @@ class SnowflakeQueryEntry extends EntryExtension {
       ParamDoc(
         "options",
         typeDoc = TypeDoc(List("list")),
-        description = """Extra connection options.""".stripMargin,
+        description = """Extra connection options, e.g. `[{"timezone", "UTC"}]`.""".stripMargin,
         isOptional = true
       )
     ),
