6eda89a4f0ad6b93d32acec3ddc49f436d450493
Miguel Branco <miguel@raw-labs.com>
2024-09-11 11:05:23 +0200

    Removing utils sources.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,6 +16,8 @@ object Dependencies {
 
   val utilsCore = "com.raw-labs" %% "utils-core" % "0.50.0"
 
+  val utilsSources = "com.raw-labs" %% "utils-sources" % "0.50.0"
+
   val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
 
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.12"
